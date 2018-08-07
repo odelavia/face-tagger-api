55ecb88b13fab0210e5be6564c86424abe2b573e
odelavia <odelavia@gmail.com>
2018-08-07 02:01:17 -0400

    set up redis for caching, and JWTS for sessions

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
     ClarifaiApiKey: process.env.CLARIFAI_API_KEY,
-    DB: process.env.DB
+    DB: process.env.DB,
+    JWT: process.env.JWT
 }
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,28 @@
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+
+// Redis Setup
+const redis = require('redis');
+// You will want to update your host to the proper address in production
+const redisClient = redis.createClient({host: '127.0.0.1'});
+
+const signToken = (username) => {
+  const jwtPayload = { username };
+  return jwt.sign(jwtPayload, 'https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c', { expiresIn: '2 days'});
+};
+
+const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
+
+const createSession = (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() => {
+      return { success: 'true', userId: id, token, user }
+    })
+    .catch(console.log);
+};
+
 const handleSignin = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -10,17 +35,36 @@ const handleSignin = (db, bcrypt) => (req, res) => {
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
+          .then(user => user[0])
           .catch(err => res.status(400).json('unable to get user'))
       } else {
-        res.status(400).json('wrong credentials')
+        return Promise.reject('wrong credentials');
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => err)
+}
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(401).send('Unauthorized');
+    }
+    return res.json({id: reply})
+  });
+}
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization ? getAuthTokenId(req, res)
+    : handleSignin(db, bcrypt, req, res)
+    .then(data =>
+      data.id && data.email ? createSession(data) : Promise.reject(data))
+    .then(session => res.json(session))
+    .catch(err => res.status(400).json(err));
 }
 
 module.exports = {
-  handleSignin: handleSignin
+  signinAuthentication: signinAuthentication,
+  redisClient: redisClient
 }
