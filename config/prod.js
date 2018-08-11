cd7833449310c3673530d3ed7e98b05675622ddb
Oliver De La Via <odelavia@gmail.com>
2018-08-10 22:35:37 -0400

    forgot port last commit

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,5 @@ module.exports = {
     ClarifaiApiKey: process.env.CLARIFAI_API_KEY,
     DB: process.env.DATABASE_URL,
     JWT: process.env.JWT,
-    Redis_PORT: process.env.REDIS_PORT
+    Redis_URL: process.env.REDIS_URL
 }
