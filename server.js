34fea5dc3dec46ca2e4bd80d9c3ba485651e56c3
Oliver De La Via <odelavia@gmail.com>
2018-08-11 00:01:50 -0400

    added missing express app.use for static client

--- server.js
+++ server.js
@@ -19,18 +19,20 @@ const db = knex({
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
 app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
 app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
 app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(process.env.PORT || 3000, ()=> {
   console.log('app is running on port 3000');
